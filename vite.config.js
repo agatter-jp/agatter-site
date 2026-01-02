51c4a10338f6a87143fa5d19c309b173615ec2b9
Naopon <info@agatter.com>
2026-01-02 09:46:40 +0900

    aboutを別ページに変更

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-    base: '/',
-})
+    base: "/", // カスタムドメインなら基本これでOK
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                about: resolve(__dirname, "about.html"),
+            },
+        },
+    },
+});
